74716dd2f87833868e7083739febddce1d9008bd
Praneeth Polati <polatipraneeth@gmail.com>
2024-08-06 14:19:51 +0530

    second commit

--- frontend/frontend/src/App.js
+++ frontend/frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import {BrowserRouter as Router,Route,Routes} from 'react-router-dom';
+import TodoList from './src/components/TodoList';
+import AddTodo from './src/components/AddTodo';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+function App(){
+    return(
+        <Router>
+            <div>
+                <Routes>
+                    <Route path ="/" element={<TodoList/>}/>
+                    <Route path ="/add" element={<AddTodo/>}/>
+                </Routes>
+            </div>
+        </Router>
+    );
 }
 
-export default App;
+export default AddTodo;
